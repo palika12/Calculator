87d261d270550c5af094d9a749be8784b7cf0390
palika mehra <palikamehra03@gmail.com>
2024-02-08 15:54:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    let expression = document.getElementById('display').value;
+    let result;
+
+    try {
+        result = eval(expression);
+        if (isNaN(result) || !isFinite(result)) {
+            throw new Error('Invalid expression');
+        }
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
